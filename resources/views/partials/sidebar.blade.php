afeb74603eae4e7926f81d83e8a9afb319fd40d7
Adnanpanakkattil <muhammedadnan1247@gmail.com>
2026-07-25 10:01:24 +0530

    sidemenu UI Desin

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,43 @@
 <aside class="sidebar">
-   <div class="card">
-    <h1>Side Bar</h1>
+   <!-- logo -->
+   <div class="sidebar-logo">
+
+      <img src="{{ asset('assets/images/Nexora-logo.png') }}"
+         alt="Nexora Logo"
+         class="logo-image">
+
+      <h3>Nexora</h3>
+
    </div>
+   <!-- menu items -->
+   <nav class="sidebar-nav">
+      <p class="menu-title">MAIN</p>
+      <ul class="menu">
+         <li>
+
+            <a href="#" class="active">
+               <i class="bi bi-grid"></i>
+               Dashboard
+            </a>
+
+         </li>
+
+         <li>
+            <a href="#">
+               <i class="bi bi-bar-chart"></i>
+               <span>Analytics</span>
+            </a>
+         </li>
+
+         <li>
+            <a href="#">
+               <i class="bi bi-file-earmark-text"></i>
+               <span>Reports</span>
+            </a>
+         </li>
+
+      </ul>
+
+   </nav>
+
 </aside>
